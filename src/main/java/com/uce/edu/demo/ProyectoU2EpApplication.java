19c389d68a40f61bea4b582d8caee600c1b802fe
ec98 <carlospiruch98@gmail.com>
2022-06-26 19:08:24 -0500

    Configurando log4j para imprimir

--- src/main/java/com/uce/edu/demo/ProyectoU2EpApplication.java
+++ src/main/java/com/uce/edu/demo/ProyectoU2EpApplication.java
@@ -4,9 +4,15 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//imports logs
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @SpringBootApplication
 public class ProyectoU2EpApplication implements CommandLineRunner{
 
+	public static final Logger Logger = LoggerFactory.getLogger(ProyectoU2EpApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoU2EpApplication.class, args);
 	}
@@ -14,7 +20,8 @@ public class ProyectoU2EpApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-		System.out.println("HOLA MUNDO");
+		
+		Logger.info("Funciando log4j [HOLA MUNDO]");
 	}
 
 }
