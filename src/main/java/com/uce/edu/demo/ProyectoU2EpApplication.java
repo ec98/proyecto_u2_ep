579815f5a7afe31e2121697b4f36e37ac9c30c4c
ec98 <carlospiruch98@gmail.com>
2022-07-25 20:25:30 -0500

    Ejercicios ManyToMany con la funcionalida Set<name>

--- src/main/java/com/uce/edu/demo/ProyectoU2EpApplication.java
+++ src/main/java/com/uce/edu/demo/ProyectoU2EpApplication.java
@@ -1,5 +1,8 @@
 package com.uce.edu.demo;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,9 +10,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.uce.edu.demo.repository.modelo.onetomany.Conjunto;
-import com.uce.edu.demo.service.ICasaService;
-import com.uce.edu.demo.service.IConjuntoService;
+import com.uce.edu.demo.repository.modelo.manytomany.Autor;
+import com.uce.edu.demo.repository.modelo.manytomany.Libro;
+import com.uce.edu.demo.service.ILibroService;
 
 @SpringBootApplication
 public class ProyectoU2EpApplication implements CommandLineRunner {
@@ -17,10 +20,7 @@ public class ProyectoU2EpApplication implements CommandLineRunner {
 	private static final Logger Logger = LoggerFactory.getLogger(ProyectoU2EpApplication.class);
 
 	@Autowired
-	private IConjuntoService iConjuntoService;
-
-	@Autowired
-	private ICasaService iCasaService;
+	private ILibroService iLibroService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoU2EpApplication.class, args);
@@ -29,46 +29,20 @@ public class ProyectoU2EpApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-		// Imprimiendo solo la tabla maestro -> "Conjunto"
+		Autor autor1 = new Autor();
+		autor1.setNombre("Junior Marco");
+		
+		Set<Autor> autores = new HashSet<>();
+		autores.add(autor1);
+		
+		Libro libro = new Libro();
+		libro.setTitulo("Capitan America");
+		libro.setCantidadPaginas(200);
+		
+		libro.setAutores(autores);
+	
+		this.iLibroService.insert(libro);
 
-		Conjunto conjunto = new Conjunto();
-		conjunto.setNombre("Joel Ambart");
-		conjunto.setTelefono("2929282111");
-//		conjunto.setId(1);
-
-//		Casa casa = new Casa();
-//		casa.setNumero("05");
-//		casa.setPisos("1");
-//		casa.setCosto(new BigDecimal("2211"));
-//		casa.setConjunto(conjunto);
-//
-//		Casa casa1 = new Casa();
-//		casa1.setNumero("02");
-//		casa1.setPisos("7");
-//		casa1.setCosto(new BigDecimal("22323922"));
-//		casa1.setConjunto(conjunto);
-
-		// CREATE
-		this.iConjuntoService.insertar(conjunto);
-		// this.iCasaService.insertar(casa);
-		// this.iCasaService.insertar(casa1);
-
-		// READ
-		this.iConjuntoService.buscar(7);
-		// this.iCasaService.Buscar(null);
-
-		// UPDATE
-		conjunto.setNombre("San Jose Benavides");
-		conjunto.setTelefono("29292929211");
-		this.iConjuntoService.actualizar(conjunto);
-//		 casa.setCosto(new BigDecimal("23222"));
-//		 casa1.setCosto(new BigDecimal("191911"));
-//		 this.iCasaService.actualizar(casa);
-//		 this.iCasaService.actualizar(casa1);
-
-		// DELETE
-		this.iConjuntoService.eliminar(7);
-		// this.iCasaService.eliminar(1);
 	}
 
 }
